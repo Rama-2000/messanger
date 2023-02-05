8f8ecee47eeb05148fb316e32e311947c051c86f
Rama-2000 <rahmatullo9984@gmail.com>
2023-02-05 23:47:48 +0500

    Staring Part

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        Logo Header
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div className='content'>
+        <div>
+            <img  src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/02/10-Things-You-Didnt-Know-About-Naruto-Uzumaki.png" alt="" />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Profile
